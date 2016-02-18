8226bd06a60b0568253f376e978ebe18be1dbe28
realazthat <realazthat@gmail.com>
2016-02-18 13:48:08 -0500

    Changed Findllvm.cmake to match convention
    - cached vars should be singular, regular variables should accumulate the cached variables and be plural

--- cmake/Findllvm.cmake
+++ cmake/Findllvm.cmake
@@ -7,41 +7,51 @@
 # LLVM_LIBRARIES
 # LLVM_LIBDIRS
 
-find_path(LLVM_BASE_INCLUDE_DIR NAMES llvm/IR/IRBuilder.h PATHS /usr/include/llvm-3.7/)
-find_path(LLVM_C_INCLUDE_DIR NAMES llvm-c/Core.h PATHS /usr/include/llvm-c-3.7/)
-set(LLVM_INCLUDE_DIRS ${LLVM_C_INCLUDE_DIR} ${LLVM_BASE_INCLUDE_DIR}
-      CACHE PATH "List of directories to add to the include path for LLVM")
+find_path(LLVM_BASE_INCLUDE_DIR NAMES llvm/IR/IRBuilder.h PATHS /usr/include/llvm-3.7/ DOC "List of directories to add to the include path for LLVM")
+find_path(LLVM_C_INCLUDE_DIR NAMES llvm-c/Core.h PATHS /usr/include/llvm-c-3.7/ DOC "List of directories to add to the include path for LLVM C library")
+
+set(LLVM_INCLUDE_DIR ${LLVM_C_INCLUDE_DIR} ${LLVM_BASE_INCLUDE_DIR})
+
 
 find_program(LLVM_CONFIG_EXE NAMES llvm-config llvm-config-3.7 DOC "Path to llvm-config for LLVM")
 
 
-if(NOT LLVM_LIBRARIES)
+if(NOT LLVM_BASE_LIBRARY)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --libs
       OUTPUT_VARIABLE LLVM_LIBRARIES_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_LIBRARIES "${LLVM_LIBRARIES_COMPUTED}" CACHE STRING "List of LLVM libraries to use when linking against LLVM")
+  set(LLVM_BASE_LIBRARY "${LLVM_LIBRARIES_COMPUTED}" CACHE STRING "List of LLVM libraries to use when linking against LLVM")
+  mark_as_advanced(LLVM_BASE_LIBRARY)
 endif()
 
-if(NOT LLVM_SYSTEM_LIBS)
+if(NOT LLVM_SYSTEM_LIBRARY)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --system-libs
       OUTPUT_VARIABLE LLVM_SYSTEM_LIBS_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_SYSTEM_LIBS "${LLVM_SYSTEM_LIBS_COMPUTED}" CACHE STRING "List of system libraries to use when linking against LLVM")
+  set(LLVM_SYSTEM_LIBRARY "${LLVM_SYSTEM_LIBS_COMPUTED}" CACHE STRING "List of system libraries to use when linking against LLVM")
+  mark_as_advanced(LLVM_SYSTEM_LIBRARY)
 endif()
 
-if(NOT LLVM_LIBDIRS)
+if(NOT LLVM_LIBDIR)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --libdir
       OUTPUT_VARIABLE LLVM_LIBDIRS_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_LIBDIRS "${LLVM_LIBDIRS_COMPUTED}" CACHE PATH "List of directories where the LLVM libraries live")
+  set(LLVM_LIBDIR "${LLVM_LIBDIRS_COMPUTED}" CACHE PATH "List of directories where the LLVM libraries live")
+  mark_as_advanced(LLVM_LIBDIR)
 endif()
+
+
 
 
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LLVM DEFAULT_MSG LLVM_LIBRARIES LLVM_INCLUDE_DIRS LLVM_LIBDIRS)
 
-mark_as_advanced(LLVM_INCLUDE_DIRS LLVM_LIBRARIES LLVM_LIBDIRS)
+if(LLVM_FOUND)
+  set(LLVM_LIBDIRS ${LLVM_LIBDIR})
+  set(LLVM_LIBRARIES ${LLVM_SYSTEM_LIBRARY} ${LLVM_BASE_LIBRARY})
+  set(LLVM_INCLUDE_DIRS ${LLVM_INCLUDE_DIR})
+endif()

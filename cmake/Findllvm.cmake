c7f4868398c2873174fd4812186fb0613c0a97f8
realazthat <realazthat@gmail.com>
2016-02-18 20:32:02 -0500

    CMake: Fixups from the merge, unified and made consistent some cmake vars

--- cmake/Findllvm.cmake
+++ cmake/Findllvm.cmake
@@ -12,33 +12,27 @@
 #
 # Variables used by the module, overridable on the commandline:
 # LLVM_CONFIG_EXE - path to llvm-config, if specified, everything else is implicit; cmake will attempt to find this
-# LLVM_BASE_INCLUDE_DIR - path(s) to llvm include directory(s)
-# LLVM_C_INCLUDE_DIR - path(s) to llvm-c include directory(s)
+# LLVM_INCLUDE_DIR - path(s) to llvm include directory(s)
 # LLVM_LIBRARY_DIR - path(s) to llvm library directories
 # LLVM_BASE_LIBRARY - list of llvm libraries to link against
 # LLVM_SYSTEM_LIBRARY - list of system libraries to link against, required by llvm
 #
 
 
-find_program(LLVM_CONFIG_EXE NAMES llvm-config llvm-config-3.7 DOC "Path to llvm-config for LLVM; if specified, many of the other llvm-related vars are implicit")
+find_program(LLVM_CONFIG_EXE NAMES llvm-config llvm-config-3.7 DOC "FULL Path to llvm-config for LLVM; if specified, many of the other llvm-related vars are implicit")
 
 
-#NOTE: TODO: find this with llvm-config??
-find_path(LLVM_INCLUDE_DIR NAMES llvm-c/Core.h PATHS /usr/include/llvm-c-3.7/ DOC "List of directories to add to the include path for LLVM library")
-mark_as_advanced(LLVM_BASE_INCLUDE_DIR LLVM_C_INCLUDE_DIR)
 
-set(LLVM_LIBRARIES ${LLVM_LIBRARIES} ${LLVM_SYSTEM_LIBS})
 if(NOT LLVM_INCLUDE_DIR)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --includedir
       OUTPUT_VARIABLE LLVM_INCLUDE_DIR_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_INCLUDE_DIR_COMPUTED "${LLVM_INCLUDE_DIR_COMPUTED}" CACHE STRING "List of LLVM libraries to use when linking against LLVM")
+  set(LLVM_INCLUDE_DIR "${LLVM_INCLUDE_DIR_COMPUTED}" CACHE STRING "List of LLVM include directories to use when compiling against LLVM")
   mark_as_advanced(LLVM_INCLUDE_DIR)
 endif()
 
 
-#NOTE: TODO: Why do we not have a separate LLVM_C_LIBRARY? 
 if(NOT LLVM_BASE_LIBRARY)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --libs
@@ -51,24 +45,20 @@ endif()
 if(NOT LLVM_SYSTEM_LIBRARY)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --system-libs
-      OUTPUT_VARIABLE LLVM_SYSTEM_LIBS_COMPUTED
+      OUTPUT_VARIABLE LLVM_SYSTEM_LIB_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_SYSTEM_LIBRARY "${LLVM_SYSTEM_LIBS_COMPUTED}" CACHE STRING "List of system libraries to use when linking against LLVM")
+  set(LLVM_SYSTEM_LIBRARY "${LLVM_SYSTEM_LIB_COMPUTED}" CACHE STRING "List of system libraries to use when linking against LLVM")
   mark_as_advanced(LLVM_SYSTEM_LIBRARY)
 endif()
 
 if(NOT LLVM_LIBRARY_DIR)
   execute_process(
       COMMAND ${LLVM_CONFIG_EXE} --libdir
-      OUTPUT_VARIABLE LLVM_LIBDIRS_COMPUTED
+      OUTPUT_VARIABLE LLVM_LIBRARY_DIR_COMPUTED
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(LLVM_LIBRARY_DIR "${LLVM_LIBDIRS_COMPUTED}" CACHE PATH "List of directories where the LLVM libraries live")
+  set(LLVM_LIBRARY_DIR "${LLVM_LIBRARY_DIR_COMPUTED}" CACHE PATH "List of directories where the LLVM libraries live")
   mark_as_advanced(LLVM_LIBRARY_DIR)
 endif()
-
-
-
-
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(llvm

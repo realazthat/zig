7693442e3da6ede3742de43f23460f24f26f3985
realazthat <realazthat@gmail.com>
2016-03-08 21:34:36 -0600

    Travis CI: replaced obsolete clang-modernize with clang-tidy package

--- ci/install-llvm-38-from-repos.sh
+++ ci/install-llvm-38-from-repos.sh
@@ -7,4 +7,4 @@ wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -qq
-sudo apt-get install -y clang-3.8 clang-3.8-doc libclang-common-3.8-dev libclang-3.8-dev libclang1-3.8 libclang1-3.8-dbg libllvm-3.8-ocaml-dev libllvm3.8 libllvm3.8-dbg lldb-3.8 llvm-3.8 llvm-3.8-dev llvm-3.8-doc llvm-3.8-examples llvm-3.8-runtime clang-modernize-3.8 clang-format-3.8 python-clang-3.8 lldb-3.8-dev
+sudo apt-get install -y clang-3.8 clang-3.8-doc libclang-common-3.8-dev libclang-3.8-dev libclang1-3.8 libclang1-3.8-dbg libllvm-3.8-ocaml-dev libllvm3.8 libllvm3.8-dbg lldb-3.8 llvm-3.8 llvm-3.8-dev llvm-3.8-doc llvm-3.8-examples llvm-3.8-runtime clang-tidy-3.8 clang-format-3.8 python-clang-3.8 lldb-3.8-dev
